6eefefc79ac3667694f8edcecc325d883d27121f
DVisTaskOne <daniilv23@mail.ru>
2024-02-27 21:01:31 +0300

    markup

---
+++ lsb/src/main/java/ru/dvis/LSB.java
@@ -0,0 +1,12 @@
+package ru.dvis;
+
+public class LSB {
+
+    public static byte[] encode(byte[] file, byte[] msg) {
+        return file;
+    }
+    public static byte[] decode(byte [] file) {
+        return file;
+    }
+
+}
