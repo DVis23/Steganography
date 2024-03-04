242f8f6ae97efcf615c636fc565a2a120d505b9f
DVisTaskOne <daniilv23@mail.ru>
2024-03-04 13:09:36 +0300

    lsb v2.2 with ETB

--- lsb/src/main/java/ru/dvis/LSB.java
+++ lsb/src/main/java/ru/dvis/LSB.java
@@ -1,7 +1,10 @@
 package ru.dvis;
 
-import java.awt.*;
+import org.apache.commons.lang3.ArrayUtils;
 import java.awt.image.BufferedImage;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.BitSet;
 
 public class LSB {
@@ -12,10 +15,13 @@ public class LSB {
         int width = img.getWidth();
         int height = img.getHeight();
 
-        int index = 0;
+        // Добавление в сообщение символа окончания блока
+        msg = ArrayUtils.add(msg, (byte)23);
 
+        int index = 0;
         for (int i = width - 1; i >= 0; i--) {
             for (int j = height - 1; j >= 0; j--) {
+                // Получение значение пикселя в RGB
                 int pixel = img.getRGB(i, j);
 
                 int [] rgb = new int[] {(pixel & 0xff0000) >> 16, (pixel & 0xff00) >> 8, (pixel & 0xff)};
@@ -23,9 +29,36 @@ public class LSB {
                     if (index < msg.length * 8) {
                         rgb[k] = replaceBit(rgb[k], getBit(msg, index), 0);
                         index++;
-                    } else {
-                        img.setRGB(i, j, (rgb[0] & 0xff) << 16 | (rgb[1] & 0xff) << 8 | (rgb[2] & 0xff));
-                        return img;
+                    }
+                }
+                img.setRGB(i, j, (rgb[0] & 0xff) << 16 | (rgb[1] & 0xff) << 8 | (rgb[2] & 0xff));
+            }
+        }
+        return img;
+    }
+
+    // Внедрение сообщения
+    public static BufferedImage encodeImage(BufferedImage img, String msg) {
+        // Получение ширины и высоты изображение
+        int width = img.getWidth();
+        int height = img.getHeight();
+
+        // Перевод строки в массив байтов
+        byte[] msgByte = msg.getBytes(StandardCharsets.US_ASCII);
+        // Добавление в сообщение символа окончания блока
+        msgByte = ArrayUtils.add(msgByte, (byte)23);
+
+        int index = 0;
+        for (int i = width - 1; i >= 0; i--) {
+            for (int j = height - 1; j >= 0; j--) {
+                // Получение значение пикселя в RGB
+                int pixel = img.getRGB(i, j);
+
+                int [] rgb = new int[] {(pixel & 0xff0000) >> 16, (pixel & 0xff00) >> 8, (pixel & 0xff)};
+                for (int k = 0; k < 3; k++) {
+                    if (index < msgByte.length * 8) {
+                        rgb[k] = replaceBit(rgb[k], getBit(msgByte, index), 0);
+                        index++;
                     }
                 }
                 img.setRGB(i, j, (rgb[0] & 0xff) << 16 | (rgb[1] & 0xff) << 8 | (rgb[2] & 0xff));
@@ -35,34 +68,34 @@ public class LSB {
     }
 
     // Извлечение сообщения
-    public static byte[] decodeImage(BufferedImage img, int msgSize) {
+    public static String decodeImage(BufferedImage img) {
         // Получение ширины и высоты изображение
         int width = img.getWidth();
         int height = img.getHeight();
 
-        int index = 0;
-        BitSet bitsetMsg = new BitSet();
+        // Множество бит извлекаемого сообщения
+        ArrayList<Boolean> bits = new ArrayList<Boolean>();
         for (int i = width - 1; i >= 0; i--) {
             for (int j = height - 1; j >= 0; j--) {
+                // Получение значение пикселя в RGB
                 int pixel = img.getRGB(i, j);
 
                 int [] rgb = new int[] {(pixel & 0xff0000) >> 16, (pixel & 0xff00) >> 8, (pixel & 0xff)};
-
                 for (int k = 0; k < 3; k++) {
-                    if (index < msgSize) {
+                    if (!checkETB(bits)) {
                         rgb[k] = (rgb[k] & 1);
-                        if (rgb[k] == 1) bitsetMsg.set(index);
-                        index++;
-                    } else return reverse(bitsetMsg.toByteArray());
+                        if (rgb[k] == 1) bits.add(true);
+                        else bits.add(false);
+                    }
                 }
-
             }
         }
-        return reverse(bitsetMsg.toByteArray());
+        return new String(bitsToBytes(bits));
     }
 
-    private static int replaceBit(int num, boolean bit, int position) {
-        int mask = 1 << position;
+    // Замена бита в байте по индексу
+    private static int replaceBit(int num, boolean bit, int bitIndex) {
+        int mask = 1 << bitIndex;
         // Если новый бит равен 1, то устанавливаем его
         if (bit) {
             num |= mask;
@@ -73,20 +106,36 @@ public class LSB {
         return num;
     }
 
-    private static byte[] reverse(byte [] bytes) {
-        for (int i = 0; i < bytes.length; i++) {
-            bytes[i] = (byte) (Integer.reverse(bytes[i]) >>> 24);
-        }
-        return bytes;
-    }
-
-    public static boolean getBit(byte[] array, int bitIndex) {
+    // Получение бита из массива байтов по индексу
+    private static boolean getBit(byte[] bytes, int bitIndex) {
         int byteIndex = bitIndex / 8;
         int bitOffset = bitIndex % 8;
 
-        if (byteIndex >= array.length) return false;
+        if (byteIndex >= bytes.length) return false;
 
-        byte targetByte = array[byteIndex];
+        byte targetByte = bytes[byteIndex];
         return ((targetByte >> (7 - bitOffset)) & 1) == 1;
+    }
+
+    // Проверка на символ окончания блока
+    private static boolean checkETB(ArrayList<Boolean> bits) {
+        if (bits.size() % 8 != 0 || bits.size() == 0) return false;
+        // ETB-символ 00010111
+        else return  bits.get(bits.size() - 1) && bits.get(bits.size() - 2) && bits.get(bits.size() - 3) &&
+                !bits.get(bits.size() - 4) && bits.get(bits.size() - 5) &&
+                !bits.get(bits.size() - 6) && !bits.get(bits.size() - 7) && !bits.get(bits.size() - 8);
+    }
+
+    private static byte[] bitsToBytes(ArrayList<Boolean> bits) {
+        byte [] bytes = new byte[bits.size() / 8 - 1];
+        for (int i = 0; i < bits.size() - 8; i+=8) {
+            StringBuilder sb = new StringBuilder();
+            for (int j = 0; j < 8; j++) {
+                if (bits.get(i + j)) sb.append("1");
+                else sb.append("0");
+            }
+            bytes[i / 8] = Byte.parseByte(sb.toString(), 2);
+        }
+        return bytes;
     }
 }
--- src/main/java/ru/dvis/Main.java
+++ src/main/java/ru/dvis/Main.java
@@ -10,20 +10,19 @@ public class Main {
 
     public static void main(String [] args) throws IOException {
 
-        String msg = "Hello World!!!";
-        System.out.println(msg);
-        int msgSize = msg.length() * 8;
+        String msg = "The Byte class wraps a value of primitive type byte in an object. \n" +
+                "In addition, this class provides several methods for converting a byte to a String and a String to a byte, " +
+                "as well as other constants and methods useful when dealing with a byte.\n" +
+                "\n" +
+                "For example, in a future release, synchronization may fail.";
 
-        byte[] msgByte = msg.getBytes(StandardCharsets.US_ASCII);
         BufferedImage img = ImageIO.read(new File("image/2.jpg"));
-        BufferedImage lsbImg = LSB.encodeImage(img, msgByte);
+        BufferedImage lsbImg = LSB.encodeImage(img, msg);
 
         try {
             ImageIO.write(lsbImg, "png", new File("image/lsb.png"));
-
             BufferedImage msgImg = ImageIO.read(new File("image/lsb.png"));
-            byte[] decodeMsgByte = LSB.decodeImage(msgImg, msgSize);
-            String decodeMsg = new String(decodeMsgByte);
+            String decodeMsg = LSB.decodeImage(msgImg);
             System.out.println(decodeMsg);
         } catch (IOException ignored) {
         }
